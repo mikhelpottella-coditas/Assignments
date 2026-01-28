b95ed4042be320e4d060a7746192ac8e96998a87
mikhelPottella-coditas <mikhel.pottella@coditas.com>
2026-01-28 10:38:46 +0530

    1st question in assignment -         //1. In a program, input the side of a square. You have to output the area of the square

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//import java.util.Arrays;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//
+//        short smallNum = 4567;
+////        long bigNum= 3456789+num;
+////        byte shorestNum = 126;
+////
+////        double withDecimal = 3.2323;
+////        float forSmallDecimal = 5.453f+bigNum;
+////
+////        boolean binary = true;
+//
+//        char letter ;
+////        String word = "h\fe\tl\bo";
+//        System.out.println(Arrays.toString(a));
+//
+//
+//    }
+//}
---
+++ src/assignments/assignment1.java
@@ -0,0 +1,21 @@
+package assignments;
+//1. In a program, input the side of a square. You have to output the area of the square
+// input  : 2 (side of square)  output : 4 (area of that square)
+
+public class assignment1 {
+    public static void main(String[] args) {
+
+
+        //1. In a program, input the side of a square. You have to output the area of the square
+        // input  : 2 (side of square)  output : 4 (area of that square)
+        System.out.println(squareArea(40));
+
+    }
+
+    //1. In a program, input the side of a square. You have to output the area of the square
+    // input  : 2 (side of square)  output : 4 (area of that square)
+    private static int squareArea(int n){
+        return n*n;
+    }
+
+}
