cd7e5c57421e0d450d0369bca07bcb3ee1b963c1
mikhelPottella-coditas <mikhel.pottella@coditas.com>
2026-01-28 10:46:13 +0530

    2nd question in assingment In a program, input 3 numbers : compute the average of three numbers

---
+++ src/assignments/Average.java
@@ -0,0 +1,11 @@
+package assignments;
+
+public class Average {
+    public static void main(String[] args) {
+        System.out.println(averageNum(1,2,3));
+    }
+
+    private static double averageNum(int a, int b,int c){
+        return (a+b+c)/3;
+    }
+}
--- src/assignments/Square.java
+++ src/assignments/Square.java
@@ -2,13 +2,13 @@ package assignments;
 //1. In a program, input the side of a square. You have to output the area of the square
 // input  : 2 (side of square)  output : 4 (area of that square)
 
-public class assignment1 {
+public class Square {
     public static void main(String[] args) {
 
 
         //1. In a program, input the side of a square. You have to output the area of the square
         // input  : 2 (side of square)  output : 4 (area of that square)
-        System.out.println(squareArea(40));
+        System.out.println(squareArea(680));
 
     }
 
